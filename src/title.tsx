a233942120ddd26681c62bde65f4b044ddebbe6c
alongo <amandaylongo@gmail.com>
2025-03-30 23:26:16 -0400

    Changes by katherine

---
+++ src/title.tsx
@@ -0,0 +1,8 @@
+
+export default function Title() {
+    return (
+        <div className={"title"}>
+            <h1>CS3733-D25 Team 1: Nail Art 💅 & Ballroom Dance 💃</h1>
+        </div>
+    );
+}
