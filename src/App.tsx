f9cd6113613faa0685fe8422454b1bed71ee6902
kat24-4 <ktse2424@yahoo.com>
2025-04-01 01:38:16 -0400

    added hobby stuff into " "2.tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@
 import './App.css'
 import Title from "./title.tsx";
 import Intro from "./intro.tsx";
+import Hobby2 from "./hobby2.tsx";
+import List2 from "./list2.tsx";
+import Table2 from "./table2.tsx";
+import Form2 from "./form2.tsx";
 
     function App() {
       //const [count, setCount] = useState(0)
@@ -12,6 +16,11 @@ import Intro from "./intro.tsx";
                     <Title/>
                     <Intro name1 = "Amanda"
                            name2 = "Katherine"/>
+
+                    <Hobby2/>
+                    <List2/>
+                    <Table2/>
+                    <Form2/>
                 </>
             );
     }
